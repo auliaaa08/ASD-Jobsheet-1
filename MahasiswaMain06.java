4590c1069c9f2852b48e8983c79a764638b3380a
auliaaa08 <ab.auliaabilqis@gmail.com>
2026-02-20 09:42:36 +0700

    Percobaan 3: Modifikasi class MahasiswaMain06 untuk menambahkan object dengan menggunakan konstruktor berparameter pada pertanyaan nomor 2

--- MahasiswaMain06.java
+++ MahasiswaMain06.java
@@ -10,5 +10,11 @@ public class MahasiswaMain06 {
     mhs1.ubahKelas("SI 2K");
     mhs1.updateIpk(3.60);
     mhs1.tampilkanInformasi();
+
+    Mahasiswa06 mhs2 = new Mahasiswa06("Annisa Nabila", "2141720169", 3.25, "TI 2L");
+
+    mhs2.updateIpk(3.30);
+    mhs2.tampilkanInformasi();
+
     }
 }
